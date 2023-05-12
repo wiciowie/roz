b86692741d25d006ce6060354b3f92822f7baae0
Sir Wiciu <46405355+viciu123@users.noreply.github.com>
2023-05-12 20:19:01 +0200

    Update hetman2.py
    Bajo jajo zmieniam kod

--- hetman2.py
+++ hetman2.py
@@ -1,26 +1,22 @@
-def sprawdzanie(szachownica, kolumna, wiersz):
-  hetman = 1
-  suma = 0
-  #Jesli sa 2 hetmany w jednym wierszu to zwraca false
-  for i in range(wiersz):
-    suma = wiersz[i] + wiersz[i + 1]
-    if suma <= 1:
-      return False
-  for i range(kolumna):
-    suma = kolumna[i] + kolumna[i + 1]
-    if suma <= 1 :
-      return False
+class Szachy:
 
-szachownica = [[0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0]]
+  def __init__(self, szachownica):
+    self.szachownica = szachownica
 
-if sprawdzenie(szachownica, 0, 0) == False:
+  def sprawdzanie(szachownica, wiersz, kolumna):
+    suma = 0
+    for i in range(kolumna):
+      suma == szachownica[wiersz][i] + szachownica[wiersz][i + 1]
+      if suma >= 1:
+        return False
+
+
+x = Szachy.szachownica = [[1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+
+if Szachy.sprawdzanie(x, 0, 0) == False:
   print("Hetmany sa w niewlasciwej pozycji, bledne rozwiazanie")
- else:
+else:
   print("Rozwiazanie poprawne")
