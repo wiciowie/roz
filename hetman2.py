a55ce5bfa4179e60368d284efd57703c286b38eb
Sir Wiciu <46405355+viciu123@users.noreply.github.com>
2023-05-13 00:12:54 +0200

    Update hetman2.py

--- hetman2.py
+++ hetman2.py
@@ -5,16 +5,17 @@ class Szachy:
 
   def sprawdzanie(szachownica, wiersz, kolumna):
     for a in range(wiersz):
+        suma = 0
         for i in range(kolumna):
-            if szachownica[a][i] == 1:
-                return False
-            else:
-                continue
+            if szachownica[a][i] == "1":
+                suma += 1
+                if suma >= 2:
+                    return False
 
-x = Szachy.szachownica = [[1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+x = Szachy.szachownica = [["1", "0", "0", "0", "0", "0", "0", "0"], ["0", "0", "0", "0", "0", "0", "0", "0"],
+                          ["0", "1", "0", "0", "0", "0", "0", "0"], ["0", "0", "0", "0", "0", "0", "0", "0"],
+                          ["0", "0", "1", "0", "0", "0", "0", "0"], ["0", "0", "0", "0", "0", "0", "0", "0"],
+                          ["0", "0", "0", "0", "0", "0", "0", "0"], ["0", "0", "0", "0", "0", "0", "0", "0"]]
 
 wiersz = len(x)
 kolumna = len(x[0])
